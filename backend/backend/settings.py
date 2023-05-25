d7c44be7ce35593544b2cbc12f47618579044bf4
OlgaBeznutrova <olga.beznutrova@gmail.com>
2023-05-25 13:33:11 +1200

    Add workflow with PEP8 linting and setup.cfg file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,4 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
